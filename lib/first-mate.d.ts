1741a5ff591ba6cb30ebaae5e6d4975df1a173ae
Homa Wong <homawong@gmail.com>
2017-05-04 14:04:24 -0700

    Update first-mate.d.ts

--- lib/first-mate.d.ts
+++ lib/first-mate.d.ts
@@ -3,7 +3,7 @@ import GrammarRegistry = require('./grammar-registry');
 import ScopeSelector = require('./scope-selector');
 import GrammarEventHandler = require('./grammar-event-handler');
 
-export {Grammar}
-export {GrammarRegistry}
-export {ScopeSelector}
-export {GrammarEventHandler}
+export {Grammar};
+export {GrammarRegistry};
+export {ScopeSelector};
+export {GrammarEventHandler};
